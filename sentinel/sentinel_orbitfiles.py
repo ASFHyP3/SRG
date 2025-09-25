1420716b0acec9fc66f77b922e4fe28a34cae64f
Andrew Johnston <asjohnston@alaska.edu>
2025-09-25 12:26:06 -0800

    use check_call instead of run so exceptions are raised for download errors

--- sentinel/sentinel_orbitfiles.py
+++ sentinel/sentinel_orbitfiles.py
@@ -14,4 +14,4 @@ if len(sys.argv) < 1:
 for file in os.listdir("."):
     if file.endswith(".zip"):
         scene = file.strip('.zip')
-        subprocess.run(['wget', '--content-disposition', f'https://s1-orbits.asf.alaska.edu/scene/{scene}'])
+        subprocess.check_call(['wget', '--content-disposition', f'https://s1-orbits.asf.alaska.edu/scene/{scene}'])
